1a6cac2cd4c4897078668e9b7fab731f2b0eb883
xtherexuu <therexuu@gmail.com>
2023-04-21 00:02:43 +0200

    added currencies array to project

---
+++ src/App/currencies.js
@@ -0,0 +1,22 @@
+const currencies = [
+    {
+        name: "Polski Złoty",
+        short: "PLN",
+        rate: 1,
+    },
+    {
+        name: "Euro",
+        short: "EUR",
+        rate: 1,
+    },
+    {
+        name: "Dolar Amerykański",
+        short: "USD",
+        rate: 1,
+    },
+    {
+        name: "Funt Brytyjski",
+        short: "GBP",
+        rate: 1,
+    },
+]
